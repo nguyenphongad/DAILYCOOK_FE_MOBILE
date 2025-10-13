b6aef6341473f71bb86a166f3145320489c61331
phong <nguyenphonghk2003@gmail.com>
2025-10-13 21:42:19 +0700

    #6 fix UI page meal detail stack

---
+++ app/(stacks)/_layout.jsx
@@ -0,0 +1,92 @@
+import React from 'react';
+import { Stack, useRouter } from 'expo-router';
+import { TouchableOpacity, Text, View, StyleSheet } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
+// Component HeaderLeft để sử dụng cho nút quay lại
+const HeaderLeft = ({ goBack, title, onGoBack }) => {
+  const router = useRouter();
+  const insets = useSafeAreaInsets();
+  
+  const handleGoBack = () => {
+    if (typeof onGoBack === 'function') {
+      onGoBack();
+    } else {
+      router.push(goBack || '/');
+    }
+  };
+  
+  return (
+    <TouchableOpacity 
+      style={[styles.headerLeftContainer, { marginLeft: 10 }]} 
+      onPress={handleGoBack}
+    >
+      <Ionicons name="chevron-back" size={24} color="#FFFFFF" />
+      {title ? <Text style={styles.headerLeftTitle}>{title}</Text> : null}
+    </TouchableOpacity>
+  );
+};
+
+export default function StackLayout() {
+  const router = useRouter();
+
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+        animation: 'slide_from_right', // Đổi kiểu animation để nhanh hơn
+        headerTitleAlign: 'left',
+        headerLeft: () => (
+          <HeaderLeft
+            goBack="/(tabs)"
+            title=""
+            onGoBack=""
+          />
+        ),
+        headerTitleStyle: {
+          fontWeight: 'bold',
+          fontSize: 18,
+          color: "#fff",
+        },
+        headerStyle: {
+          backgroundColor: '#35A55E', 
+        },
+      }}
+    >
+      
+      {/* Meal Routes */}
+      <Stack.Screen
+        name="meals/MealDetail"
+        options={{
+          title: 'Chi tiết món ăn',
+        }}
+      />
+
+
+    {/* các màn hfinh stack ghi ở day toạ router */}
+
+
+
+
+
+
+
+
+
+    </Stack>
+  );
+}
+
+const styles = StyleSheet.create({
+  headerLeftContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  headerLeftTitle: {
+    color: '#FFFFFF',
+    fontSize: 16,
+    marginLeft: 5,
+    fontWeight: '500',
+  }
+});
